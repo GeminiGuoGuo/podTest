3f2148a4bfa440c227a5eb9635d70c88a6fcc161
lc <gqy@guoqingyangdeMacBook-Pro.local>
2018-05-17 16:59:34 +0800

    add project

---
+++ podTest/podTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  podTest
+//
+//  Created by gqy on 2018/5/17.
+//  Copyright © 2018年 guoqingyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
